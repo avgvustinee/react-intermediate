63c0f742ac329c797c766fba2471bcc03db052b1
codewithsyre <augustineshoks7@gmail.com>
2024-11-05 22:10:56 +0200

    Infinite Queries

--- src/reactQuery/hooks/usePosts.ts
+++ src/reactQuery/hooks/usePosts.ts
@@ -1,39 +1,37 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useInfiniteQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
-export interface Post {
-    id: number;
-    title: string;
-    body: string;
-    userId: number;
+interface Post {
+	id: number;
+	title: string;
+	body: string;
+	userId: number;
 }
 
 interface PostQuery {
-    page: number;
-    pageSize: number;
+	pageSize: number;
 }
 
 const usePosts = (query: PostQuery) => {
-    const fetchPosts = () =>
-        axios
-            .get<Post[]>('https://jsonplaceholder.typicode.com/posts', {
-                params: {
-                    _start: (query.page - 1) * query.pageSize,
-                    _limit: query.pageSize
+	const fetchPosts = (pageParam) =>
+		axios
+			.get<Post[]>('https://jsonplaceholder.typicode.com/posts', {
+				params: {
+					_start: (pageParam - 1) * query.pageSize,
+					_limit: query.pageSize,
+				},
+			})
+			.then((res) => res.data);
 
-                }
-            })
-            .then((res) => res.data);
+	return useInfiniteQuery<Post[], Error>({
+		queryKey: ['posts', query],
+		queryFn: ({ pageParam = 1 }) => fetchPosts(pageParam),
+		staleTime: 1 * 60 * 1000, // 1m
+		initialPageParam: 1,
+		getNextPageParam: (lastPage, allPages) => {
+			return lastPage.length > 0 ? allPages.length + 1 : undefined;
+		},
+	});
+};
 
-    return useQuery<Post[], Error>({
-        queryKey: ['posts', query],
-        queryFn: fetchPosts,
-        staleTime: 1 * 60 * 1000,
-
-
-
-    });
-}
-
-
-export default usePosts
+export default usePosts;
