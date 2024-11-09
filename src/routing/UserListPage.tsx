09a35e0976e7f70ef295687cb19a0a5f158e6dde
codewithsyre <augustineshoks7@gmail.com>
2024-11-09 18:57:48 +0200

    Passing Data with Route Parameters

--- src/routing/UserListPage.tsx
+++ src/routing/UserListPage.tsx
@@ -1,18 +1,20 @@
+import { Link } from "react-router-dom";
+
 const UserListPage = () => {
-    const users = [
-      { id: 1, name: 'Augustine' },
-      { id: 2, name: 'Dianna' },
-      { id: 3, name: 'Akira' },
-    ];
-    return (
-      <ul className="list-group">
-        {users.map((user) => (
-          <li className="list-group-item" key={user.id}>
-            <a href="#">{user.name}</a>
-          </li>
-        ))}
-      </ul>
-    );
-  };
-  
-  export default UserListPage;
+  const users = [
+    { id: 1, name: "Augustine" },
+    { id: 2, name: "Dianna" },
+    { id: 3, name: "Akira" },
+  ];
+  return (
+    <ul className="list-group">
+      {users.map((user) => (
+        <li className="list-group-item" key={user.id}>
+          <Link to={`/users/${user.id}`}>{user.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserListPage;
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import ContactPage from "./ContactPage";
 import HomePage from "./HomePage";
+import UserDetailPage from "./UserDetailPage";
 import UserListPage from "./UserListPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/users", element: <UserListPage /> },
   { path: "/contact", element: <ContactPage /> },
+  { path: "/users/:id", element: <UserDetailPage /> },
 ]);
 
 export default router;
