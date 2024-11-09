3921e50bbd9882584a2e09306eac3a0b9b16fc65
codewithsyre <augustineshoks7@gmail.com>
2024-11-09 10:25:37 +0200

    Managing Application State with Zustand.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
-import HomePage from "./stateManagement/components/HomePage";
+import HomePage from "./stateManagement/HomePage";
 import NavBar from "./stateManagement/NavBar";
 import { AuthProvider } from "./stateManagement/auth";
+import Counter from "./stateManagement/counter/Counter";
 import { TasksProvider } from "./stateManagement/tasks";
 
 function App() {
   return (
     <AuthProvider>
       <TasksProvider>
+        <Counter />
         <NavBar />
         <HomePage />
       </TasksProvider>
